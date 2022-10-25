6c00f6600a4acb5f53da12033ddcdfa4e119755d
DanielEscobaar <descobar@inspalamos.cat>
2022-10-25 17:57:30 +0200

    fda

--- M6_UF2_A2/src/main/java/Main.java
+++ M6_UF2_A2/src/main/java/Main.java
@@ -1,6 +1,10 @@
 import Funcions.FuncionsUsuari;
 import Funcions.FuncionsViatjes;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Scanner;
 
 public class Main
@@ -109,7 +113,18 @@ public class Main
     {
     }
 
-    private static void conexioDB()
+    private static void conexioDB() throws Exception
     {
+        String url = "jdbc:mysql://localhost:3306/m6_uf2_a1";         // url on esta la base de dades
+        String user = "Daniel";                            // usuari base de dades
+        String password = "1234";              // contrasenya base de dades
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection connection = DriverManager.getConnection(url, user, password);
+        Statement statement = connection.createStatement();
+        ResultSet rs = statement.executeQuery("Select * from persones");
+
+        while(rs.next()){
+            
+        }
     }
 }
