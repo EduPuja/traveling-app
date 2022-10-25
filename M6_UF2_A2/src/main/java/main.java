9daa8d77301d6a46a3f0c0bd6ef854d858c6ca38
DanielEscobaar <descobar@inspalamos.cat>
2022-10-25 16:33:47 +0200

    tots els menus en el main

--- M6_UF2_A2/src/main/java/main.java
+++ M6_UF2_A2/src/main/java/main.java
@@ -6,6 +6,133 @@ public class main
 
     public static void main(String[] args)
     {
+        conexioDB();
+        boolean menu = false;
+        do{
+            System.out.println("Entra una opcio: ");
+            System.out.println("1 - Gestio d'Usuaris");
+            System.out.println("2 - Gestio de Viatges");
+            System.out.println("3 - Compra Bitllets");
+            System.out.println("0 - Sortir");
+            
+            int opcio = lector.nextInt();
+            lector.nextLine();
+            
+            switch (opcio){
+                case 1:
+                        menuUsuaris();
+                    break;
+                case 2:
+                        menuViatges();
+                    break;
+                case 3:
+                        menuBitllets();
+                    break;
+                case 0:
+                        System.out.println("Adeu.");
+                        menu = true;
+                    break;
+                default:
+                    break;
+            }
+        }while(!menu);
+    }
+    private static void menuUsuaris()
+    {
+        boolean menu = false;
+        do{
+            System.out.println("Entra una opcio: ");
+            System.out.println("1 - Alta Usuari");
+            System.out.println("2 - Baixa Usuari");
+            System.out.println("3 - Modificar Usuari");
+            System.out.println("0 - Sortir");
 
+            int opcio = lector.nextInt();
+            lector.nextLine();
+
+            switch (opcio){
+                case 1:
+                    altaUser();
+                    break;
+                case 2:
+                    baixaUser();
+                    break;
+                case 3:
+                    modificacioUser();
+                    break;
+                case 0:
+                    System.out.println("Adeu.");
+                    menu = true;
+                    break;
+                default:
+                    break;
+            }
+        }while(!menu);
+    }
+
+    private static void altaUser()
+    {
+    }
+
+    private static void baixaUser()
+    {
+    }
+
+    private static void modificacioUser()
+    {
+    }
+
+
+    private static void menuViatges()
+    {
+        boolean menu = false;
+        do{
+            System.out.println("Entra una opcio: ");
+            System.out.println("1 - Nou Viatje");
+            System.out.println("2 - Eliminar Viatje");
+            System.out.println("3 - Modificar Viatje");
+            System.out.println("0 - Sortir");
+
+            int opcio = lector.nextInt();
+            lector.nextLine();
+
+            switch (opcio){
+                case 1:
+                    nouViatje();
+                    break;
+                case 2:
+                    eliminarViatje();
+                    break;
+                case 3:
+                    modificarViatje();
+                    break;
+                case 0:
+                    System.out.println("Adeu.");
+                    menu = true;
+                    break;
+                default:
+                    break;
+            }
+        }while(!menu);
+    }
+
+    private static void nouViatje()
+    {
+    }
+
+    private static void eliminarViatje()
+    {
+    }
+
+    private static void modificarViatje()
+    {
+    }
+
+    private static void menuBitllets()
+    {
+    }
+
+    private static void conexioDB()
+    {
     }
 }
---
+++ M6_UF2_A2/src/main/java/objectes/Viatje.java
@@ -0,0 +1,5 @@
+package objectes;
+
+public class Viatje
+{
+}
