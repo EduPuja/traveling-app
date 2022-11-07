8f77d51480d5b920c0f5308822e68db64e34af1c
Eduard <edpufa@inspalamos.cat>
2022-11-07 17:14:01 +0100

    Canvio de Conneixo
    Mejor usuari admin

--- M6_UF2_A2/src/main/java/Funcions/ConnexioBDD.java
+++ M6_UF2_A2/src/main/java/Funcions/ConnexioBDD.java
@@ -5,20 +5,23 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-public class ConnexioBDD {
+public class ConnexioBDD
+{
 
-    public static void conexioDB() throws Exception
+    public static Statement conexioDB() throws Exception
     {
-        String url = "jdbc:mysql://localhost:3306/m6_uf2_a1";         // url on esta la base de dades
-        String user = "Daniel";                            // usuari base de dades
-        String password = "1234";              // contrasenya base de dades
+        // url on esta la base de dades
+        String url = "jdbc:mysql://192.168.18.184:3306/m6_uf2_a1";
+        // usuari base de dades
+        String user = "admin";
+        // contrasenya base de dades
+        String password = "123asd123";
+        // Driver
         Class.forName("com.mysql.jdbc.Driver");
         Connection connection = DriverManager.getConnection(url, user, password);
         Statement statement = connection.createStatement();
-        ResultSet rs = statement.executeQuery("Select * from persones");
 
-        while (rs.next()) {
+        return statement;
 
-        }
     }
 }
