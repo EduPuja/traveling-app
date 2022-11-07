e2150c304fa158cec4a0822b158a7a88c8cbab64
Eduard <edpufa@inspalamos.cat>
2022-11-07 18:25:35 +0100

    Better Liniafactura
    Afegir un construcotr buit java doc
    i un metode general tant set com get

--- M6_UF2_A2/src/main/java/objectes/Linia_Factura.java
+++ M6_UF2_A2/src/main/java/objectes/Linia_Factura.java
@@ -2,13 +2,24 @@ package objectes;
 
 public class Linia_Factura
 {
-    int numLinia;
-    int idPersona;
-    double preu;
+    private int numLinia;
+    private int idPersona;
+    private double preu;
 
-    public Linia_Factura(){
+    /**
+     * Construcor buit
+     */
+    public Linia_Factura()
+    {
 
     }
+
+    /**
+     * Construcotr with params
+     * @param numLinia
+     * @param idPersona
+     * @param preu
+     */
     public Linia_Factura(int numLinia, int idPersona, double preu)
     {
         this.numLinia = numLinia;
@@ -16,31 +27,53 @@ public class Linia_Factura
         this.preu = preu;
     }
 
+    /**
+     * Metode general per afegir objecte liniaFactura
+     * @param numLinia
+     * @param idPersona
+     * @param preu
+     */
+    public void setAllLiniaFact(int numLinia, int idPersona, double preu)
+    {
+        this.numLinia = numLinia;
+        this.idPersona = idPersona;
+        this.preu = preu;
+    }
+
+    /**
+     * Metode per retornar tota la info de una linaFactura
+     * @return
+     */
+    public String getInfoLinaFact()
+    {
+        return "NUM_LINIA:"+this.numLinia+"\nID_PERSONA: "+this.idPersona+"\nPREU: "+this.preu;
+    }
+
+    //** GETS **//
+
     public int getNumLinia()
     {
         return numLinia;
     }
-
-    public void setNumLinia(int numLinia)
-    {
-        this.numLinia = numLinia;
-    }
-
     public int getIdPersona()
     {
         return idPersona;
     }
-
-    public void setIdPersona(int idPersona)
-    {
-        this.idPersona = idPersona;
-    }
-
     public double getPreu()
     {
         return preu;
     }
 
+
+    // ** SETS **//
+    public void setNumLinia(int numLinia)
+    {
+        this.numLinia = numLinia;
+    }
+    public void setIdPersona(int idPersona)
+    {
+        this.idPersona = idPersona;
+    }
     public void setPreu(double preu)
     {
         this.preu = preu;
