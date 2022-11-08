0f079bff9929807e43d357f7c74c42edb332b848
Eduard <edpufa@inspalamos.cat>
2022-11-08 18:09:55 +0100

    Recogiendo la conexion

--- M6_UF2_A2/src/main/java/Implements/ImplementsViatjes.java
+++ M6_UF2_A2/src/main/java/Implements/ImplementsViatjes.java
@@ -4,18 +4,26 @@ import Funcions.ConnexioBDD;
 import Interfaces.InterfaceViatjes;
 import objectes.Viatje;
 
+import java.sql.Statement;
+
 public class ImplementsViatjes implements InterfaceViatjes
 {
-    public void nouViatje(Viatje vNou) throws Exception {
-        ConnexioBDD.conexioDB();
+    public void nouViatje(Viatje vNou) throws Exception
+    {
+       Statement con =ConnexioBDD.conexioDB();
+
+
     }
-    public void eliminarViatje(Viatje vEliminar) throws Exception {
-        ConnexioBDD.conexioDB();
+    public void eliminarViatje(Viatje vEliminar) throws Exception
+    {
+       Statement statement=  ConnexioBDD.conexioDB();
     }
-    public void modificarViatje(Viatje vUpdate) throws Exception {
-        ConnexioBDD.conexioDB();
+    public void modificarViatje(Viatje vUpdate) throws Exception
+    {
+        Statement stat= ConnexioBDD.conexioDB();
     }
-    public void llistarViatje() throws Exception {
-        ConnexioBDD.conexioDB();
+    public void llistarViatje() throws Exception
+    {
+        Statement statement =ConnexioBDD.conexioDB();
     }
 }
