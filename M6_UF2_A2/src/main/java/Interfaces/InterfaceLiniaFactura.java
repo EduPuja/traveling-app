5c2dbb821336be0db44dc4cf1347e0c935f5daac
EduPuja <edpufa@inspalamos.cat>
2022-11-08 23:58:36 +0100

    Creacio de interface liniafactura
    Aqui tienes la interficie de linia factura

---
+++ M6_UF2_A2/src/main/java/Interfaces/InterfaceLiniaFactura.java
@@ -0,0 +1,13 @@
+package Interfaces;
+
+import objectes.Billet;
+import objectes.Linia_Factura;
+import objectes.Viatje;
+
+public interface InterfaceLiniaFactura
+{
+    public void llistarLiniaFactura() throws Exception;
+    public void crearLiniaFactura(Linia_Factura creaLF) throws Exception;
+    public void eliminarLiniaFactura(Linia_Factura deleteLF) throws Exception;
+    public void modificarLiniaFactura(Linia_Factura updateLF) throws Exception;
+}
