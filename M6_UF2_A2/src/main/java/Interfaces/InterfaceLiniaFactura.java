2edbdc7f8f9a70fcd2b1f296caadec145f3f4145
EduPuja <edpufa@inspalamos.cat>
2022-11-09 00:36:50 +0100

    modificarLinafactura(Stirng)
    He copiat com modificar dades de estacio

--- M6_UF2_A2/src/main/java/Interfaces/InterfaceLiniaFactura.java
+++ M6_UF2_A2/src/main/java/Interfaces/InterfaceLiniaFactura.java
@@ -9,5 +9,5 @@ public interface InterfaceLiniaFactura
     public void llistarLiniaFactura() throws Exception;
     public void crearLiniaFactura(Linia_Factura creaLF) throws Exception;
     public void eliminarLiniaFactura(Linia_Factura deleteLF) throws Exception;
-    public void modificarLiniaFactura(Linia_Factura updateLF) throws Exception;
+    public void modificarLiniaFactura(String dades) throws Exception;
 }
