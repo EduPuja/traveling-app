837e6c8e052470efef0441489b716c96aa642baa
Eduard <edpufa@inspalamos.cat>
2022-11-08 18:43:32 +0100

    Metodes from estacio

---
+++ M6_UF2_A2/src/main/java/Funcions/DadesEstacio.java
@@ -0,0 +1,34 @@
+package Funcions;
+
+import objectes.Estacio;
+
+public class DadesEstacio
+{
+    /**
+     * Metode per entrar una estacio
+     */
+    public void fromAltaEstacio()
+    {
+        Estacio estacio = new Estacio();
+
+
+    }
+
+    /**
+     * Metode per donar de baixa una estacio
+     */
+    public void fromBaixaEstacio()
+    {
+
+    }
+
+    /**
+     * Metode per modifcar una estacio en aquest cas nomes sera la descripcio
+     */
+    public void modificacioEstacio()
+    {
+
+    }
+
+
+}
