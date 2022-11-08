ea4962fc23efc4035c1daed18600f6cf90915948
EduPuja <edpufa@inspalamos.cat>
2022-11-09 00:37:31 +0100

    Creacio de implementFactura
    En esta version estan todos los metodos menos el modifcar ! PUEDE QUE SE TENGAN QUE CANVIAR ALGUNOS HACIA MEJRO :dog:

---
+++ M6_UF2_A2/src/main/java/Implements/ImplementsLiniaFactura.java
@@ -0,0 +1,112 @@
+package Implements;
+
+import Funcions.ConnexioBDD;
+import Interfaces.InterfaceLiniaFactura;
+import objectes.Linia_Factura;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class ImplementsLiniaFactura implements InterfaceLiniaFactura
+{
+
+    /**
+     * Metode per llistar totes les linia factura de la taula
+     * @throws Exception
+     */
+    public void llistarLiniaFactura() throws Exception
+    {
+        Statement con = ConnexioBDD.conexioDB();
+        String query= "SELECT * FROM `linia_factura`";
+
+        ResultSet rs = con.executeQuery(query);
+        while (rs.next())
+        {
+            System.out.println("LINIA_FACUTRA: "+rs.getInt("linia_factura"));
+            System.out.println("NUM_FACTURA: "+rs.getInt("num_factura"));
+            System.out.println("ID_PERSONA : "+rs.getInt("tipus_billet"));
+            System.out.println("PREU: "+rs.getInt("preu"));
+            System.out.println("");
+        }
+        con.close();    // tencar la conexio
+
+
+    }
+
+    /**
+     * Metode per insertar una linia factura
+     * @param creaLF
+     * @throws Exception
+     */
+    public void crearLiniaFactura(Linia_Factura creaLF) throws Exception
+    {
+        Statement con = ConnexioBDD.conexioDB();
+        String query = "INSERT INTO `linia_factura`(`linia_factura`, `num_factura`, `id_persona`, `preu`) VALUES ("+creaLF.getLiniaFactura()+","+creaLF.getNumLinia()+","+creaLF.getIdPersona()+","+creaLF.getPreu()+")";
+        if(con.executeUpdate(query) == 1)
+        {
+            System.out.println("Se ha insertat una Linia factura :D");
+        }
+        else System.out.println("No se ha insertat D: la linia factura");
+    }
+
+    /**
+     * metode per eliminar una liniafactura de la taula
+     * @param deleteLF
+     * @throws Exception
+     */
+
+    public void eliminarLiniaFactura(Linia_Factura deleteLF) throws Exception
+    {
+        Statement con = ConnexioBDD.conexioDB();
+        if(consultarLiniaFactura(deleteLF.getLiniaFactura()))
+        {
+            // exgisteix llavors haig de fer el delelte
+            String query = "DELETE FROM `linia_factura` WHERE lina_facutra="+deleteLF.getLiniaFactura();
+            if(con.executeUpdate(query) == 1)
+            {
+                System.out.println("Se ha eliminat una LinaFactura");
+            }
+            else System.out.println("No se ha eliminat");
+        }
+        else System.out.println("Lina factura no trobada");
+    }
+
+    /**
+     * Metode per modificar la linia factura
+     * @param updateLF
+     * @throws Exception
+     */
+    public void modificarLiniaFactura(String dades) throws Exception
+    {
+
+        Statement statement = ConnexioBDD.conexioDB();
+        // EXEMPLE MODIFICAR estacio
+
+        /*Statement statement = ConnexioBDD.conexioDB();
+
+
+        String taula [] = dades.split("/");
+        int idEstacio = Integer.parseInt(taula[0]);
+        String desNou = taula[1];
+
+        String query = "UPDATE `estacio` SET `descrip`='"+ desNou+"' WHERE `id_estacio`= "+ idEstacio;
+
+        if(statement.executeUpdate(query) == 1) System.out.println("Modificat Correcatament.");
+        else System.out.println("Modificat Incorrectament.");
+        statement.close();;*/
+    }
+
+    public static boolean consultarLiniaFactura(int linaFactura) throws Exception
+    {
+        Statement con = ConnexioBDD.conexioDB();
+        String query = "Select linia_factura from linia_factura where linia_factura="+linaFactura;
+
+        ResultSet resultSet = con.executeQuery(query);
+        if(resultSet.next())
+        {
+            con.close();
+            return true;
+        }
+        else con.close(); return false;
+    }
+}
