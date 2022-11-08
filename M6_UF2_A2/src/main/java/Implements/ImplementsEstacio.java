18b5bf08ba2265caadd088705c481cfa40bde3ed
Eduard <edpufa@inspalamos.cat>
2022-11-08 18:10:42 +0100

    Creacio del implements Estacio
    Amb els seus metodes de les interficies

---
+++ M6_UF2_A2/src/main/java/Implements/ImplementsEstacio.java
@@ -0,0 +1,31 @@
+package Implements;
+
+import Interfaces.InterfaceEstacio;
+import objectes.Estacio;
+import objectes.Viatje;
+
+public class ImplementsEstacio implements InterfaceEstacio
+{
+
+    public void novaEstacio(Estacio eNou) throws Exception
+    {
+
+    }
+
+
+    public void deleteEstacio(Estacio eDelete) throws Exception
+    {
+
+    }
+
+    public void modificarEstacio(Estacio eUpdate) throws Exception
+    {
+
+    }
+
+
+    public void llistarEstacio() throws Exception
+    {
+
+    }
+}
