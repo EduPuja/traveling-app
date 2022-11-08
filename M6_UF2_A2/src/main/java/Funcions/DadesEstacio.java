cf0adec8d21b5f16cf509b9dada855d61689f59d
Eduard <edpufa@inspalamos.cat>
2022-11-08 19:01:06 +0100

    Alta estacio

--- M6_UF2_A2/src/main/java/Funcions/DadesEstacio.java
+++ M6_UF2_A2/src/main/java/Funcions/DadesEstacio.java
@@ -1,15 +1,44 @@
 package Funcions;
 
+import Implements.ImplementsEstacio;
+import Interfaces.InterfaceEstacio;
 import objectes.Estacio;
+
+import java.util.Scanner;
 
 public class DadesEstacio
 {
     /**
      * Metode per entrar una estacio
      */
-    public void fromAltaEstacio()
+    public void fromAltaEstacio() throws Exception
     {
-        Estacio estacio = new Estacio();
+        Scanner lector = new Scanner(System.in);
+
+
+
+        boolean okaEstacio = false;
+
+        while (!okaEstacio)
+        {
+            System.out.println("Digues el idEstacio que vols crear");
+            int idEstacio = lector.nextInt();
+            lector.nextLine();
+
+
+            if (!ImplementsEstacio.comprovarEstacio(idEstacio))
+            {
+                System.out.println("Escriu una petita descripcio");
+                String descrip = lector.nextLine();
+
+                Estacio estacio = new Estacio(idEstacio,descrip);
+
+                okaEstacio = true;
+            } else System.out.println("Aquesta estacio ja exgisteix torna a escriure");
+
+
+        }
+        lector.close();
 
 
     }
