2279d545c5a0f3ad0b6125f89327e09cf06393d2
EduPuja <edpufa@inspalamos.cat>
2022-11-10 00:12:25 +0100

    alta linafactura done ✔️

--- M6_UF2_A2/src/main/java/Funcions/DadesLiniaFactura.java
+++ M6_UF2_A2/src/main/java/Funcions/DadesLiniaFactura.java
@@ -11,22 +11,43 @@ public class DadesLiniaFactura
     private static Scanner lector = new Scanner(System.in);
 
 
-    public static void fromAltaLinaFactura(Linia_Factura factura) throws Exception
+    /**
+     * metode per donar d'alta una lina factura
+     * @throws Exception
+     */
+    public static void fromAltaLinaFactura() throws Exception
     {
 
 
 
+        System.out.println("Digues el numero de la lina factura");
+        int linaFactnum = lector.nextInt();
+        lector.nextLine();
+        System.out.println("Digues el num de la factura");
+        int numFactura =  lector.nextInt();
+        lector.nextLine();
+        System.out.println("Digues el id de la persona a qui va a la lina factura");
+        int idPersona = lector.nextInt();
+        lector.nextLine();
+        //todo comprovar que aquest id estigui realment a la basse de dades sino esta et surti un error
+        System.out.println("Digues un preu a la linafactura");
+        int preuLinaFactura = lector.nextInt();
+        lector.nextLine();
+
+
+        Linia_Factura linaFact = new Linia_Factura(linaFactnum,numFactura,idPersona,preuLinaFactura); // objecte
+
     }
-    public static void fromBaixaLinaFactura(Linia_Factura factura) throws Exception
+    public static void fromBaixaLinaFactura() throws Exception
     {
 
     }
-    public static void fromDeleteLinaFactura(Linia_Factura factura) throws Exception
+    public static void fromDeleteLinaFactura() throws Exception
     {
 
     }
 
-    public static void fromUpdateLinaFactura(Linia_Factura factura) throws Exception
+    public static void fromUpdateLinaFactura() throws Exception
     {
         
     }
