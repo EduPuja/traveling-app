6c2d8e7f43765adbc956babec266841238755295
EduPuja <edpufa@inspalamos.cat>
2022-11-09 01:21:28 +0100

    Create DadesLiniaFactura.java
    Creacio dades linafactura Nomes mestodes no doing nothing

---
+++ M6_UF2_A2/src/main/java/Funcions/DadesLiniaFactura.java
@@ -0,0 +1,30 @@
+package Funcions;
+
+import java.sql.SQLSyntaxErrorException;
+import java.util.Scanner;
+
+public class DadesLiniaFactura
+{
+    private static Scanner lector = new Scanner(System.in);
+
+
+    public static void fromAltaLinaFactura() throws Exception
+    {
+
+    }
+    public static void fromBaixaLinaFactura() throws Exception
+    {
+
+    }
+    public static void fromDeleteLinaFactura() throws Exception
+    {
+
+    }
+
+    public static void fromUpdateLinaFactura() throws Exception
+    {
+        
+    }
+
+
+}
