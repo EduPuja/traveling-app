be616816eef100b75e82baa5a65c89f1bd438eb4
EduPuja <edpufa@inspalamos.cat>
2022-11-09 23:57:44 +0100

    millorant la dadesLiniaFactura
    fent insert

--- M6_UF2_A2/src/main/java/Funcions/DadesLiniaFactura.java
+++ M6_UF2_A2/src/main/java/Funcions/DadesLiniaFactura.java
@@ -1,6 +1,9 @@
 package Funcions;
 
+import Objectes.Linia_Factura;
+
 import java.sql.SQLSyntaxErrorException;
+import java.sql.Statement;
 import java.util.Scanner;
 
 public class DadesLiniaFactura
@@ -8,23 +11,34 @@ public class DadesLiniaFactura
     private static Scanner lector = new Scanner(System.in);
 
 
-    public static void fromAltaLinaFactura() throws Exception
+    public static void fromAltaLinaFactura(Linia_Factura factura) throws Exception
+    {
+        Statement con = ConnexioBDD.conexioDB();
+        String query = "INSERT INTO `linia_factura`(`linia_factura`, `num_factura`, `id_persona`, `preu`) VALUES ("+factura.getLiniaFactura()+","+factura.getNumLinia()+","+factura.getIdPersona()+","+factura.getPreu()+")";
+
+        if(con.executeUpdate(query) == 1)
+        {
+            System.out.println("Linia Factura se ha inserit");
+        }
+        else System.out.println("Linfactura No insertada");
+
+
+    }
+    public static void fromBaixaLinaFactura(Linia_Factura factura) throws Exception
     {
 
     }
-    public static void fromBaixaLinaFactura() throws Exception
-    {
-
-    }
-    public static void fromDeleteLinaFactura() throws Exception
+    public static void fromDeleteLinaFactura(Linia_Factura factura) throws Exception
     {
 
     }
 
-    public static void fromUpdateLinaFactura() throws Exception
+    public static void fromUpdateLinaFactura(Linia_Factura factura) throws Exception
     {
         
     }
+
+
 
 
 }
