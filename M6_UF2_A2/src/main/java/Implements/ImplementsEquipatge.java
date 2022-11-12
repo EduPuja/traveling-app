f4c401acf24b506160148a9e21d97cede18a75e4
EduPuja <edpufa@inspalamos.cat>
2022-11-12 17:51:11 +0100

    Equipatge llistra baseadades

--- M6_UF2_A2/src/main/java/Implements/ImplementsEquipatge.java
+++ M6_UF2_A2/src/main/java/Implements/ImplementsEquipatge.java
@@ -4,6 +4,7 @@ import Funcions.ConnexioBDD;
 import Interfaces.InterfaceEquipatge;
 import Objectes.Equipatge;
 
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class ImplementsEquipatge implements InterfaceEquipatge
@@ -30,20 +31,57 @@ public class ImplementsEquipatge implements InterfaceEquipatge
     }
 
     @Override
-    public void eliminarEquipatge(int idEquipatge)
+    public void eliminarEquipatge(int idEquipatge)throws Exception
     {
+        Statement con = ConnexioBDD.conexioDB();
 
+
+
+        con.close();
     }
 
     @Override
-    public void modificarEquipatge(String dades)
+    public void modificarEquipatge(String dades) throws Exception
     {
+        Statement con = ConnexioBDD.conexioDB();
 
+
+
+        con.close();
     }
 
+    /**
+     * Llistar equipatge ✅
+     * @throws Exception
+     */
     @Override
-    public void llistarEquipatge()
+    public void llistarEquipatge()throws Exception
     {
+        Statement con = ConnexioBDD.conexioDB();
+        String query = "SELECT * FROM `equipatge`";
 
+        ResultSet rs = con.executeQuery(query);
+
+
+        while (rs.next())
+        {
+           System.out.println("ID_Equip: "+rs.getInt("id_equip"));
+            System.out.println("Num_FACTURA: "+rs.getInt("num_factura"));
+            System.out.println("Lina_Factura: "+rs.getInt("lina_factura"));
+            System.out.println("Pes_kg: "+rs.getInt("pes_kg"));
+            System.out.println("Num_Maletes: "+rs.getInt("num_maletes"));
+            System.out.println("");
+        }
+
+        rs.close();
+        con.close();
+    }
+
+    public static void comprovarEquipatge(int idEquipatge) throws Exception
+    {
+        Statement con = ConnexioBDD.conexioDB();
+
+
+        con.close();
     }
 }
